6229f811e1a731b4c600acfc9a2e903b962e490e
iamprajyot <iamprajyot@github.com>
2014-03-19 02:12:42 +0530

    All implementation regarding the click event is done but on continuous mousedown event show numbers only is still pending

--- public_html/js/calculator.js
+++ public_html/js/calculator.js
@@ -1,14 +1,41 @@
 var prev_button = null;
 var prev_time = 0;
+var click_count = 0;
+
+var arr = [
+    ['1', '.', ',', '!'],
+    ['2', 'a', 'b', 'c'],
+    ['3', 'd', 'e', 'f'],
+    ['4', 'g', 'h', 'i'],
+    ['5', 'j', 'k', 'l'],
+    ['6', 'm', 'n', 'o'],
+    ['7', 'p', 'q', 'r', 's'],
+    ['8', 't', 'u', 'v'],
+    ['9', 'w', 'x', 'y', 'z']
+];
+
+
+
 jQuery(document).ready(function() {
-    jQuery("#phone").find("button").mouseup(function(event) {
+    jQuery("#phone").find("button").click(function(event) {
         var button_pressed = $(event.currentTarget).data("value");
         var current_time = (new Date()).getTime();
-        if (button_pressed === prev_button)
+        if (isNaN(button_pressed) || button_pressed === 0)
         {
-            sameButtonClick(prev_time, current_time);
+            jQuery("#result").val(t9(jQuery("#result").val(), button_pressed));
         }
-        jQuery("#result").val(t9(jQuery("#result").val(), button_pressed));
+        else {
+            if (button_pressed === prev_button)
+            {
+                click_count++;
+                sameButtonClick(prev_time, current_time, button_pressed);
+            }
+            else
+            {
+                click_count = 1;
+                jQuery("#result").val(t9(jQuery("#result").val(), arr[button_pressed - 1][1]));
+            }
+        }
 
         prev_button = button_pressed;
         prev_time = current_time;
@@ -20,17 +47,28 @@ function t9(text, button_pressed) {
     return text;
 }
 
-function sameButtonClick(prevTime, currentTime)
+function earlySameButton(text, button_pressed) {
+    // Write your code here
+    text = text.toString().slice(0, -1) + button_pressed;
+    return text;
+}
+
+function sameButtonClick(prevTime, currentTime, buttonValue)
 {
     if (currentTime > (prevTime + 1000))
     {
         // repeat value
-        prevTime = currentTime;
-        console.log('late');
+        click_count = 1;
+        jQuery("#result").val(t9(jQuery("#result").val(), arr[buttonValue - 1][1]));
     }
     else
     {
         // next value
+        if (click_count > (arr[buttonValue - 1].length - 1))
+        {
+            click_count = 1;
+        }
         console.log('early');
+        jQuery("#result").val(earlySameButton(jQuery("#result").val(), arr[buttonValue - 1][click_count]));
     }
 }
